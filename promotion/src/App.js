93d2adfbb59f3f67f2c94c17d3d263eef53387de
minsgy <alstjrchl97@naver.com>
2021-02-13 12:10:41 +0900

    여러가지 수정

--- promotion/src/App.js
+++ promotion/src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import {
   Switch,
   HashRouter
- as Router, Route
+ as Router, Route, Redirect
 } from "react-router-dom"
 import MainPage from './component/MainPage';
 const App = () => {
   return (
     <Router>
-    <>
-    <Switch>
-      {/* 페이지 입장 시, 메인 페이지 컴포넌트 */}
-      <Route exact path='/' component={MainPage} />
-
-    </Switch>
-    </>
+      <>
+      <Switch>
+        {/* 페이지 입장 시, 메인 페이지 컴포넌트 */}
+        <Route exact path='/' component={MainPage} />
+        <Redirect from="*" to="/" />
+      </Switch>
+      </>
     </Router>
   );
 }
